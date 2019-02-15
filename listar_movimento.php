212e7e821f7a691a5923cab97b3ec0a5acb3c15b
iagorenato <iago.simoes@aluno.ifsp.edu.br>
2019-02-15 20:34:38 -0200

    Carregamento primeira versao locacao

---
+++ listar_movimento.php
@@ -0,0 +1,44 @@
+﻿<?php
+
+	include("menu.php");
+
+    include("conexao.php");
+
+    $consulta = "SELECT cod_veiculo, marca, modelo, ano, nome, data_inicio, data_fim, movimento.valor_diaria FROM movimento 
+	INNER JOIN cliente ON id_cliente=cod_cliente INNER JOIN veiculo ON id_veiculo=cod_veiculo";
+	
+	// mysqli_error($conexao)
+    $resultado = mysqli_query($conexao,$consulta) 
+		or die("Erro. Não foi possível consultar a lista de movimentos do sistema.");
+
+	echo "<table border='1'>";
+
+	echo "<tr>
+			<th>Nome</th>
+			<th>Marca</th>
+			<th>Modelo</th>
+			<th>Ano</th>
+			<th>Placa</th>
+			<th>Data Inicio</th>
+			<th>Data Fim</th>
+			<th>Valor Diaria</th>
+		  </tr>";
+
+    while($linha=mysqli_fetch_assoc($resultado)){     
+        echo "
+		<tr>
+			<td>" .$linha["nome"]. "</td>
+			<td>" .$linha["marca"]. "</td>
+			<td>" .$linha["modelo"]. "</td>
+			<td>" .$linha["ano"]. "</td>
+			<td>" .$linha["cod_veiculo"]. "</td>
+			<td>" .$linha["data_inicio"]. "</td>
+			<td>" .$linha["data_fim"]. "</td>
+			<td>" .$linha["valor_diaria"]. "</td>
+		</tr>
+		";
+    }
+	
+	echo "</table>";
+
+?>
